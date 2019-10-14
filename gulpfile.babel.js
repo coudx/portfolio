067e16811855dfe0573d42d4244c03d2dbbf23f7
cloud xu <xuanlinxu1004@gmail.com>
2019-10-14 00:44:00 -0700

    part 1, info filling finished

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,37 +1,38 @@
 "use strict";
 
 import gulp from "gulp";
-import browserSync from "browser-sync";
+import browsersync from "browser-sync";
 import sass from "gulp-sass";
 import uglify from "gulp-uglify";
 import cleanCSS from "gulp-clean-css";
 import plumber from "gulp-plumber";
 import concat from "gulp-concat";
 import rename from "gulp-rename";
-
-var reload = browserSync.reload;
+import del from "del";
 
 // use default task to launch Browsersync and watch JS files
-gulp.task("sync", () => {
+// gulp.task("sync", done => {
+let sync = done => {
   // Serve files from the root of this project
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    },
+  browsersync.init({
+    server: { baseDir: "./" },
     port: 3000
   });
-});
+  done();
+};
 
 // add browserSync.reload to the tasks array to make
 // all browsers reload after tasks are complete.
 
 // gulp.task("reload", done => {
-//   browserSync.reload();
-//   done;
-// });
+let reload = done => {
+  browsersync.reload();
+  done();
+};
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task("css", function() {
+// gulp.task("css", function() {
+let css = () => {
   return gulp
     .src("assets/scss/**/*.scss")
     .pipe(plumber())
@@ -47,26 +48,35 @@ gulp.task("css", function() {
     .pipe(rename({ suffix: ".min" }))
     .pipe(cleanCSS())
     .pipe(gulp.dest("assets/css"))
-    .pipe(browserSync.stream());
-});
+    .pipe(browsersync.stream());
+};
 
 // process JS files and return the stream
-gulp.task("js", function() {
-  return gulp
-    .src("assets/js/*.js")
-    .pipe(concat("index.js"))
-    .pipe(gulp.dest("js"))
-    .pipe(uglify())
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest("assets/js"))
-    .pipe(browserSync.stream());
-});
+// gulp.task("js", function() {
+let js = () => {
+  return (
+    gulp
+      // .src(["assets/js/*.js", "!assets/js/concat.js", "!assets/js/concat.min.js"])
+      .src("assets/js/custom.js")
+      // .pipe(concat("concat.js"))
+      // .pipe(gulp.dest("assets/js"))
+      .pipe(uglify())
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(gulp.dest("assets/js"))
+      .pipe(browsersync.stream())
+  );
+};
 
-gulp.task("watch", done => {
-  gulp.watch("assets/scss/**/*", () => ["css"]);
-  gulp.watch("assets/js/**/*", () => ["js"]);
-  gulp.watch("**/*.html", () => ["reload"]);
-});
+// gulp.task("watchFiles", done => {
+let watch = done => {
+  gulp.watch("assets/scss/**/*", css);
+  gulp.watch("assets/js/**/*", js);
+  gulp.watch("**/*.html", reload);
+};
 
-gulp.task("build", gulp.parallel("css", "js"));
-gulp.task("default", gulp.series("build", "sync", "watch"));
+// gulp.task("build", gulp.parallel("css", "js"));
+// gulp.task("default", gulp.series("build", gulp.parallel("sync", "watchFiles")));
+let build = gulp.parallel(css, js);
+let start = gulp.series(build, gulp.parallel(watch, sync));
+
+export { css, js, sync, build, watch, start as default };
